b24e21ad049a3fa25cc091a776ba682a83eaadbd
lizok18 <krasa535@gmail.com>
2025-12-16 20:22:19 +0300

    3

--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/user/oop5
-# Build directory: /home/user/oop5/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(PmrDoublyLinkedListTests "/home/user/oop5/build/tests")
-set_tests_properties(PmrDoublyLinkedListTests PROPERTIES  _BACKTRACE_TRIPLES "/home/user/oop5/CMakeLists.txt;32;add_test;/home/user/oop5/CMakeLists.txt;0;")
-subdirs("_deps/googletest-build")
